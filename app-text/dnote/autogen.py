dead3ed64cc42877e47c342c75674496e21d8a27
invakid404 <splashgamingbg@gmail.com>
2021-01-20 09:21:17 +0200

    Reformatted all autogens

--- app-text/dnote/autogen.py
+++ app-text/dnote/autogen.py
@@ -5,12 +5,25 @@ from packaging import version
 
 
 def get_release(release_data):
-	releases = list(filter(lambda x: x["prerelease"] is False and x["draft"] is False and x["tag_name"].startswith("cli-v"), release_data))
-	return None if not releases else sorted(releases, key=lambda x: version.parse(x["tag_name"])).pop()
+	releases = list(
+		filter(
+			lambda x: x["prerelease"] is False
+			and x["draft"] is False
+			and x["tag_name"].startswith("cli-v"),
+			release_data,
+		)
+	)
+	return (
+		None
+		if not releases
+		else sorted(releases, key=lambda x: version.parse(x["tag_name"])).pop()
+	)
 
 
 async def get_gosum_artifacts(hub, github_user, github_repo, version):
-	gosum_raw = await hub.pkgtools.fetch.get_page(f"https://github.com/{github_user}/{github_repo}/raw/{version}/go.sum")
+	gosum_raw = await hub.pkgtools.fetch.get_page(
+		f"https://github.com/{github_user}/{github_repo}/raw/{version}/go.sum"
+	)
 	gosum_lines = gosum_raw.split("\n")
 	gosum = ""
 	gosum_artifacts = []
@@ -27,7 +40,9 @@ async def get_gosum_artifacts(hub, github_user, github_repo, version):
 		module_uri = module_path + "/@v/" + module_ver[0] + "." + module_ext
 		module_file = re.sub("/", "%2F", module_uri)
 		gosum_artifacts.append(
-			hub.pkgtools.ebuild.Artifact(url="https://proxy.golang.org/" + module_uri, final_name=module_file)
+			hub.pkgtools.ebuild.Artifact(
+				url="https://proxy.golang.org/" + module_uri, final_name=module_file
+			)
 		)
 	return dict(gosum=gosum, gosum_artifacts=gosum_artifacts)
 
@@ -49,7 +64,9 @@ async def generate(hub, **pkginfo):
 		tag_name=version,
 		gosum=artifacts["gosum"],
 		artifacts=[
-			hub.pkgtools.ebuild.Artifact(url=f"https://github.com/{name}/{name}/archive/{version}.tar.gz"),
+			hub.pkgtools.ebuild.Artifact(
+				url=f"https://github.com/{name}/{name}/archive/{version}.tar.gz"
+			),
 			*artifacts["gosum_artifacts"],
 		],
 	)
--- games-board/nibbler/autogen.py
+++ games-board/nibbler/autogen.py
@@ -2,14 +2,18 @@
 
 
 def get_release(releases_data):
-	releases = list(filter(lambda x: x["prerelease"] is False and x["draft"] is False, releases_data))
+	releases = list(
+		filter(lambda x: x["prerelease"] is False and x["draft"] is False, releases_data)
+	)
 	return None if not releases else sorted(releases, key=lambda x: x["tag_name"]).pop()
 
 
 async def generate(hub, **pkginfo):
 	user = "fohristiwhirl"
 	repo = pkginfo["name"]
-	releases_data = await hub.pkgtools.fetch.get_page(f"https://api.github.com/repos/{user}/{repo}/releases", is_json=True)
+	releases_data = await hub.pkgtools.fetch.get_page(
+		f"https://api.github.com/repos/{user}/{repo}/releases", is_json=True
+	)
 	latest_release = get_release(releases_data)
 	if latest_release is None:
 		raise hub.pkgtools.ebuild.BreezyError(f"Can't find a suitable release of {repo}")
@@ -18,6 +22,10 @@ async def generate(hub, **pkginfo):
 	ebuild = hub.pkgtools.ebuild.BreezyBuild(
 		**pkginfo,
 		version=ebuild_version,
-		artifacts=[hub.pkgtools.ebuild.Artifact(url=f"https://github.com/{user}/{repo}/releases/download/{version}/nibbler-{ebuild_version}-linux.zip")],
+		artifacts=[
+			hub.pkgtools.ebuild.Artifact(
+				url=f"https://github.com/{user}/{repo}/releases/download/{version}/nibbler-{ebuild_version}-linux.zip"
+			)
+		],
 	)
 	ebuild.push()
